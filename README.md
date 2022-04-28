0837b6a8321d8cd18a0d2ca4f3e4beb86841e834
Colleen Murray <colleenkhm@gmail.com>
2022-04-28 00:22:50 -0500

    html wireframe, js pseudocode, basic css body declarations

---
+++ README.md
@@ -0,0 +1,24 @@
+# just a word
+
+## Description
+An app that randomly generates a word every two days and users can submit writing/artwork inspired by that word.
+
+AS A USER 
+I want to be provided with a random word and its definition
+So that I can let it guide my creativity and post my art
+
+GIVEN just a word
+WHEN I open the app
+THEN I am presented with a randomly-generated word and its definition
+WHEN I click on that word
+THEN I am taken to a page that displays all submitted artwork for that word
+WHEN I click on the archive
+THEN I am taken to a page that displays old words that I can click on to view submissions
+WHEN I view the submissions
+THEN I am able to scroll through a page of submissions sorted by highest snaps count
+WHEN I view a submission
+THEN I am prompted to snaps it
+WHEN I view a submission
+THEN I am presented with a link to the artist's page
+WHEN I click the link to the artists page
+THEN I am taken to a page that displays all of the artist's works from previous word prompts
---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const apiLink = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}"`;
+const word = '';
+
+// TODO: randomize id number from length of array of words in dictionary API
+// TODO: use that id number to select a random word
+// TODO: if that word is fewer than 4 letters, re-randomize (maybe narrow down data set to words fewer than 5 letters first and then randomize remaining data? feels like that would take forever)
+// TODO: populate submission cards with user submissions of stories
+// maybe less art-focused and more just story telling/journaling style?
+// suggestions accepted/stored then randomized and pulled from once a week
+// if for any reason this actually took off, increase frequency of using suggestions
+// someone could post all the photos that they associate with that word
+// or link music from spotify
+// or write about it
+// or upload a file if/when I can figure that out 
+// calendar so if you missed a word you can go back and click on the word/day fill that in
+// each profile is a calendar-style display with each day having the word of that day prominently displayed/you can click on that day and input the information
+
