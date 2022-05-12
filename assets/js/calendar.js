2e7ac4bbf71bee57034992035234c572c55d0323
Colleen Murray <colleenkhm@gmail.com>
2022-05-11 19:44:08 -0500

    installed dependencies

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,117 +1,199 @@
-import "../assets/css/calendar.css";
 import dayjs from "dayjs";
-// weekdays array
-const WEEKDAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-// loading current year
-const INITIAL_YEAR = dayjs().format("YYYY");
-// loading calendar on current month
-const INITIAL_MONTH = dayjs().format("M");
-const calendarDaysElement = document.getElementById("calendar-days");
+import "./styles.css";
 const weekday = require("dayjs/plugin/weekday");
 const weekOfYear = require("dayjs/plugin/weekOfYear");
+
 dayjs.extend(weekday);
 dayjs.extend(weekOfYear);
-// Select the calendar grid header element
+
+document.getElementById("app").innerHTML = `
+<div class="calendar-month">
+  <section class="calendar-month-header">
+    <div
+      id="selected-month"
+      class="calendar-month-header-selected-month"
+    ></div>
+    <section class="calendar-month-header-selectors">
+      <span id="previous-month-selector"><</span>
+      <span id="present-month-selector">Today</span>
+      <span id="next-month-selector">></span>
+    </section>
+  </section>
+
+  <ol
+    id="days-of-week"
+    class="day-of-week"
+  /></ol>
+
+  <ol
+    id="calendar-days"
+    class="days-grid"
+  >
+  </ol>
+</div>
+`;
+
+const WEEKDAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const TODAY = dayjs().format("YYYY-MM-DD");
+
+const INITIAL_YEAR = dayjs().format("YYYY");
+const INITIAL_MONTH = dayjs().format("M");
+
+let selectedMonth = dayjs(new Date(INITIAL_YEAR, INITIAL_MONTH - 1, 1));
+let currentMonthDays;
+let previousMonthDays;
+let nextMonthDays;
+
 const daysOfWeekElement = document.getElementById("days-of-week");
-// Loop through the array of weekdays
-WEEKDAYS.forEach(weekday => {
-  // For each item in the array, make a list item element
+
+WEEKDAYS.forEach((weekday) => {
   const weekDayElement = document.createElement("li");
-  // Append a child element inside the list item...
   daysOfWeekElement.appendChild(weekDayElement);
-  /// ...that contains the value in the array
   weekDayElement.innerText = weekday;
 });
-let currentMonthDays = createDaysForCurrentMonth(INITIAL_YEAR, INITIAL_MONTH)
-let previousMonthDays = createDaysForPreviousMonth(INITIAL_YEAR, INITIAL_MONTH, currentMonthDays[0])
-let nextMonthDays = createDaysForNextMonth(INITIAL_YEAR, INITIAL_MONTH)
 
-let days = [...this.previousMonthDays, ...this.currentMonthDays, ...this.nextMonthDays]
-const apiLink = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}"`;
-var word = '';
-var wordDisplayed = document.getElementById("word");
-var definitionDisplayed = document.getElementById("definition");
-var synonymsDisplayed = document.getElementById("related-words");
+createCalendar();
+initMonthSelectors();
 
-function getNumberOfDaysInMonth(year, month) {
-    return dayjs(`${year}-${month}-01`).daysInMonth()
-}
+function createCalendar(year = INITIAL_YEAR, month = INITIAL_MONTH) {
+  const calendarDaysElement = document.getElementById("calendar-days");
 
-function createDaysForCurrentMonth(year, month) {
-    return [...Array(getNumberOfDaysInMonth(year, month))].map((day, index) => {
-      return {
-        date: dayjs(`${year}-${month}-${index + 1}`).format("YYYY-MM-DD"),
-        dayOfMonth: index + 1,
-        isCurrentMonth: true
-      };
-    });
-}
+  document.getElementById("selected-month").innerText = dayjs(
+    new Date(year, month - 1)
+  ).format("MMMM YYYY");
 
-function getWeekday(date) {
-    return dayjs(date).weekday()
-}
+  removeAllDayElements(calendarDaysElement);
 
-function createDaysForPreviousMonth(year, month) {
-    const firstDayOfTheMonthWeekday = getWeekday(currentMonthDays[0].date);
-  
-    const previousMonth = dayjs(`${year}-${month}-01`).subtract(1, "month");
-    
-    // Account for first day of the month on a Sunday (firstDayOfTheMonthWeekday === 0)
-    const visibleNumberOfDaysFromPreviousMonth = firstDayOfTheMonthWeekday ? firstDayOfTheMonthWeekday - 1 : 6
-  
-    const previousMonthLastMondayDayOfMonth = dayjs(
-      currentMonthDays[0].date
-    ).subtract(visibleNumberOfDaysFromPreviousMonth, "day").date();
-  
-    return [...Array(visibleNumberOfDaysFromPreviousMonth)].map((day, index) => {    
-      return {
-        date: dayjs(
-          `${previousMonth.year()}-${previousMonth.month() + 1}-${previousMonthLastMondayDayOfMonth + index}`
-        ).format("YYYY-MM-DD"),
-        dayOfMonth: previousMonthLastMondayDayOfMonth + index,
-        isCurrentMonth: false
-      };
-    });
-}
+  currentMonthDays = createDaysForCurrentMonth(
+    year,
+    month,
+    dayjs(`${year}-${month}-01`).daysInMonth()
+  );
 
-function createDaysForNextMonth(year, month) {
-    const lastDayOfTheMonthWeekday = getWeekday(`${year}-${month}-${currentMonthDays.length}`)
-  
-    const visibleNumberOfDaysFromNextMonth = lastDayOfTheMonthWeekday ? 7 - lastDayOfTheMonthWeekday : lastDayOfTheMonthWeekday
-  
-    return [...Array(visibleNumberOfDaysFromNextMonth)].map((day, index) => {
-      return {
-        date: dayjs(`${year}-${Number(month) + 1}-${index + 1}`).format("YYYY-MM-DD"),
-        dayOfMonth: index + 1,
-        isCurrentMonth: false
-      }
-    })
+  previousMonthDays = createDaysForPreviousMonth(year, month);
+
+  nextMonthDays = createDaysForNextMonth(year, month);
+
+  const days = [...previousMonthDays, ...currentMonthDays, ...nextMonthDays];
+
+  days.forEach((day) => {
+    appendDay(day, calendarDaysElement);
+  });
 }
 
 function appendDay(day, calendarDaysElement) {
-    const dayElement = document.createElement("li");
-    const dayElementClassList = dayElement.classList;
-  
-    // Generic calendar day class
-    dayElementClassList.add("calendar-day");
-  
-    // Container for day of month number
-    const dayOfMonthElement = document.createElement("span");
-  
-    // Content
-    dayOfMonthElement.innerText = day.dayOfMonth;
-  
-    // Add an extra class to differentiate current month days from prev/next month days
-    if (!day.isCurrentMonth) {
-      dayElementClassList.add("calendar-day--not-current");
-    }
-  
-    // Append the element to the container element
-    dayElement.appendChild(dayOfMonthElement);
-    calendarDaysElement.appendChild(dayElement);
+  const dayElement = document.createElement("li");
+  const dayElementClassList = dayElement.classList;
+  dayElementClassList.add("calendar-day");
+  const dayOfMonthElement = document.createElement("span");
+  dayOfMonthElement.innerText = day.dayOfMonth;
+  dayElement.appendChild(dayOfMonthElement);
+  calendarDaysElement.appendChild(dayElement);
+
+  if (!day.isCurrentMonth) {
+    dayElementClassList.add("calendar-day--not-current");
   }
 
+  if (day.date === TODAY) {
+    dayElementClassList.add("calendar-day--today");
+  }
+}
 
-// function getDate(){
+function removeAllDayElements(calendarDaysElement) {
+  let first = calendarDaysElement.firstElementChild;
 
-// }
+  while (first) {
+    first.remove();
+    first = calendarDaysElement.firstElementChild;
+  }
+}
+
+function getNumberOfDaysInMonth(year, month) {
+  return dayjs(`${year}-${month}-01`).daysInMonth();
+}
+
+function createDaysForCurrentMonth(year, month) {
+  return [...Array(getNumberOfDaysInMonth(year, month))].map((day, index) => {
+    return {
+      date: dayjs(`${year}-${month}-${index + 1}`).format("YYYY-MM-DD"),
+      dayOfMonth: index + 1,
+      isCurrentMonth: true
+    };
+  });
+}
+
+function createDaysForPreviousMonth(year, month) {
+  const firstDayOfTheMonthWeekday = getWeekday(currentMonthDays[0].date);
+
+  const previousMonth = dayjs(`${year}-${month}-01`).subtract(1, "month");
+
+  // Cover first day of the month being sunday (firstDayOfTheMonthWeekday === 0)
+  const visibleNumberOfDaysFromPreviousMonth = firstDayOfTheMonthWeekday
+    ? firstDayOfTheMonthWeekday - 1
+    : 6;
+
+  const previousMonthLastMondayDayOfMonth = dayjs(currentMonthDays[0].date)
+    .subtract(visibleNumberOfDaysFromPreviousMonth, "day")
+    .date();
+
+  return [...Array(visibleNumberOfDaysFromPreviousMonth)].map((day, index) => {
+    return {
+      date: dayjs(
+        `${previousMonth.year()}-${previousMonth.month() + 1}-${
+          previousMonthLastMondayDayOfMonth + index
+        }`
+      ).format("YYYY-MM-DD"),
+      dayOfMonth: previousMonthLastMondayDayOfMonth + index,
+      isCurrentMonth: false
+    };
+  });
+}
+
+function createDaysForNextMonth(year, month) {
+  const lastDayOfTheMonthWeekday = getWeekday(
+    `${year}-${month}-${currentMonthDays.length}`
+  );
+
+  const nextMonth = dayjs(`${year}-${month}-01`).add(1, "month");
+
+  const visibleNumberOfDaysFromNextMonth = lastDayOfTheMonthWeekday
+    ? 7 - lastDayOfTheMonthWeekday
+    : lastDayOfTheMonthWeekday;
+
+  return [...Array(visibleNumberOfDaysFromNextMonth)].map((day, index) => {
+    return {
+      date: dayjs(
+        `${nextMonth.year()}-${nextMonth.month() + 1}-${index + 1}`
+      ).format("YYYY-MM-DD"),
+      dayOfMonth: index + 1,
+      isCurrentMonth: false
+    };
+  });
+}
+
+function getWeekday(date) {
+  return dayjs(date).weekday();
+}
+
+function initMonthSelectors() {
+  document
+    .getElementById("previous-month-selector")
+    .addEventListener("click", function () {
+      selectedMonth = dayjs(selectedMonth).subtract(1, "month");
+      createCalendar(selectedMonth.format("YYYY"), selectedMonth.format("M"));
+    });
+
+  document
+    .getElementById("present-month-selector")
+    .addEventListener("click", function () {
+      selectedMonth = dayjs(new Date(INITIAL_YEAR, INITIAL_MONTH - 1, 1));
+      createCalendar(selectedMonth.format("YYYY"), selectedMonth.format("M"));
+    });
+
+  document
+    .getElementById("next-month-selector")
+    .addEventListener("click", function () {
+      selectedMonth = dayjs(selectedMonth).add(1, "month");
+      createCalendar(selectedMonth.format("YYYY"), selectedMonth.format("M"));
+    });
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var currentDate = newDate().toLocaleDateString();
 function confirmNewDay() {
     //get today's date and check if it is a different date than it was last time function was run
     var date = new Date().toLocaleDateString();
@@ -37,54 +38,138 @@ function confirmNewDay() {
 // or upload a file if/when I can figure that out 
 // calendar so if you missed a word you can go back and click on the word/day fill that in
 
-document.getElementById("app").innerHTML = `
-<!-- Parent container for the calendar month -->
-<div class="calendar-month">
-  <!-- The calendar header -->
-  <section class="calendar-month-header">
-    <!-- Month name -->
-    <div
-      id="selected-month"
-      class="calendar-month-header-selected-month"
-    >
-      July 2020
-    </div>
+// document.getElementById("app").innerHTML = `
+// <!-- Parent container for the calendar month -->
+// <div class="calendar-month">
+//   <!-- The calendar header -->
+//   <section class="calendar-month-header">
+//     <!-- Month name -->
+//     <div
+//       id="selected-month"
+//       class="calendar-month-header-selected-month"
+//     >
+//       ${currentDate}
+//     </div>
 
-    <!-- Pagination -->
-    <div class="calendar-month-header-selectors">
-      <span id="previous-month-selector"><</span>
-      <span id="present-month-selector">Today</span>
-      <span id="next-month-selector">></span>
-    </div>
-  </section>
+//     <!-- Pagination -->
+//     <div class="calendar-month-header-selectors">
+//       <span id="previous-month-selector"><</span>
+//       <span id="present-month-selector">Today</span>
+//       <span id="next-month-selector">></span>
+//     </div>
+//   </section>
   
-  <!-- Calendar grid header -->
-  <ol
-    id="days-of-week"
-    class="day-of-week"
-  >
-    <li>Mon</li>
-    ...
-    <li>Sun</li>
-  </ol>
+//   <!-- Calendar grid header -->
+//   <ol
+//     id="days-of-week"
+//     class="day-of-week"
+//   >
+//     <li>Mon</li>
+//     <li>Tues</li>
+//     <li>Wed</li>
+//     <li>Thurs</li>
+//     <li>Fri</li>
+//     <li>Sat</li>
+//     <li>Sun</li>
+//   </ol>
 
-  <!-- Calendar grid -->
-  <ol
-    id="calendar-days"
-    class="date-grid"
-  >
-    <li class="calendar-day">
-      <span>
-        1
-      </span>
-      ...
-      <span>
-        29
-      </span>
-    </li>
-  </ol>
-</div>
-`;
+//   <!-- Calendar grid -->
+//   <ol
+//     id="calendar-days"
+//     class="date-grid"
+//   >
+//     <li class="calendar-day">
+//       <span>
+//         1
+//       </span>
+//       <span>
+//         2
+//       </span>
+//       <span>
+//         3
+//       </span>
+//       <span>
+//         4
+//       </span>
+//       <span>
+//         5
+//       </span>
+//       <span>
+//         6
+//       </span>
+//       <span>
+//         7
+//       </span>
+//       <span>
+//         8
+//       </span>
+//       <span>
+//         9
+//       </span>
+//       <span>
+//         10
+//       </span>
+//       <span>
+//         11
+//       </span>
+//       <span>
+//         12
+//       </span>
+//       <span>
+//         13
+//       </span>
+//       <span>
+//         14
+//       </span>
+//       <span>
+//         15
+//       </span>
+//       <span>
+//         16
+//       </span>
+//       <span>
+//         17
+//       </span>
+//       <span>
+//         18
+//       </span>
+//       <span>
+//         19
+//       </span>
+//       <span>
+//         20
+//       </span>
+//       <span>
+//         21
+//       </span>
+//       <span>
+//         22
+//       </span>
+//       <span>
+//         23
+//       </span>
+//       <span>
+//         24
+//       </span>
+//       <span>
+//         25
+//       </span>
+//       <span>
+//         26
+//       </span>
+//       <span>
+//         27
+//       </span>
+//       <span>
+//         28
+//       </span>
+//       <span>
+//         29
+//       </span>
+//     </li>
+//   </ol>
+// </div>
+// `;
 // each profile is a calendar-style display with each day having the word of that day prominently displayed/you can click on that day and input the information
 // utilize module lessons by having days that user has not submitted an entry for highlighted in a different color
 
